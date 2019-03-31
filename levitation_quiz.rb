a0e4622d73bfb611b93aa2b133656073beefcb99
Marco Miranda <marmirand@hotmail.com>
2019-03-31 10:54:49 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,3 +1,10 @@
-loop do
-	  puts "What is the spell that enacts levitation?"
+def annoying
+    loop do
+        puts "What's up, Doc?"
+        answer = gets.chomp
+        break if answer == "STOP"
+    end
+    puts "Okay, okay, jeez. I'll stop. Sorry."
 end
+
+annoying
